2fe6d52452b2cf48ceeffcbc0e8c2b30932f889d
Houyhnhnm <hykusb@bitmai.la>
2018-06-03 13:16:42 +0200

    add grammar specification file

---
+++ grammar.py
@@ -0,0 +1,56 @@
+
+footprintRules = [
+	[{
+		'span': [2],
+		'gait': ['double','triple'],
+		'foot': [[-1/2, 0, 1/2]],
+		'step': [[0, 1/2], [0, 1/2], [0, 1/2]],
+	}],
+]
+
+functionRules = [
+	{
+	'address': ['A1','B1'],
+	'transposition': [-1,0,1],
+	},
+]
+
+pathRules = [
+	{
+	'range': [2,3,4],
+	'figure': ['rampUp', 'rampDown'],
+	'tilt': ['float', 'dive'],
+	},
+]
+
+anticipationRules = [
+	['footprint'],
+	['tilt'],
+	[],
+]
+
+prolongationRules = [
+	['address','transposition'],
+	['address'],
+	['figure','range'],
+	['range'],
+	['range','tilt'],
+]
+
+phonology = Phonology()
+morphology = Morphology()
+
+for rule in footprintRules:
+	phonology.addFootprintRule(rule)
+
+for rule in functionRules:
+	phonology.addFunctionRule(rule)
+
+for rule in pathRules:
+	phonology.addPathRule(rule)
+
+for rule in anticipationRules:
+	morphology.addAnticipationRule(rule)
+
+for rule in prolongationRules:
+	morphology.addProlongationRule(rule)
